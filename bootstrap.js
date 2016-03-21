b71827bb5b481219d529869848dc4b78e3f81c4b
Noitidart <noitidart@gmail.com>
2016-03-21 16:13:32 -0700

    0.0.initial
    Setup without the submodule

---
+++ bootstrap.js
@@ -0,0 +1,59 @@
+const {classes: Cc, interfaces: Ci, manager: Cm, results: Cr, utils: Cu, Constructor: CC} = Components;
+
+Cu.import('resource://gre/modules/ctypes.jsm');
+Cu.import('resource://gre/modules/osfile.jsm'); // this gives the `OS` variable which is very useful for constants like `OS.System`, `OS.Constants.libc`, `OS.Constants.Win`. Constants missing from `.libc` and `.Win` you can define in the `CONSTS` object in the respective ostypes module
+Cu.import('resource://gre/modules/Services.jsm');
+
+var core = {
+    addon: {
+        name: 'ostypes_playground',
+        id: 'ostypes_playground@jetpack',
+        path: {
+            content: 'chrome://ostypes_playground/content',
+            modules: 'chrome://ostypes_playground/content/modules'
+        }
+    },
+    os: {
+        name: OS.Constants.Sys.Name.toLowerCase(), // possible values are here - https://developer.mozilla.org/en-US/docs/Mozilla/Developer_guide/Build_Instructions/OS_TARGET
+        toolkit: Services.appinfo.widgetToolkit.toLowerCase(),
+        xpcomabi: Services.appinfo.XPCOMABI
+    },
+    firefox: {
+        pid: Services.appinfo.processID,
+        version: Services.appinfo.version
+    }
+};
+core.os.mname = core.os.toolkit.indexOf('gtk') == 0 ? 'gtk' : core.os.name; // mname stands for modified-name // this will treat solaris, linux, unix, *bsd systems as the same. as they are all gtk based
+
+var BOOTSTRAP = this;
+
+switch (core.os.toolkit.indexOf('gtk') == 0 ? 'gtk' : core.os.name) {
+    case 'winnt':
+    case 'winmo':
+    case 'wince':
+        Services.scriptloader.loadSubScript(core.addon.path.modules + 'ostypes/ostypes_win.jsm', BOOTSTRAP);
+        break
+    case 'gtk':
+        Services.scriptloader.loadSubScript(core.addon.path.modules + 'ostypes/ostypes_x11.jsm', BOOTSTRAP);
+        break;
+    case 'darwin':
+        Services.scriptloader.loadSubScript(core.addon.path.modules + 'ostypes/ostypes_mac.jsm', BOOTSTRAP);
+        break;
+    default:
+        throw new Error('Operating system, "' + OS.Constants.Sys.Name + '" is not supported');
+}
+
+Services.scriptloader.loadSubScript(core.addon.path.modules + 'ostypes/cutils.jsm', BOOTSTRAP);
+Services.scriptloader.loadSubScript(core.addon.path.modules + 'ostypes/ctypes_math.jsm', BOOTSTRAP);
+
+function install() {}
+function uninstall() {}
+
+function startup(aData, aReason) {
+	Services.prompt.alert(null, 'hi', 'hi');
+}
+
+function shutdown(aData, aReason) {
+	if (aReason == APP_SHUTDOWN) { return }
+
+}
