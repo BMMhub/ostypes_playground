6bf6d18494810dba0eb56d4d23a69e0b5308ca37
Noitidart <noitidart@gmail.com>
2016-04-08 23:28:48 -0700

    ok set up

--- bootstrap.js
+++ bootstrap.js
@@ -28,6 +28,8 @@ core.os.mname = core.os.toolkit.indexOf('gtk') == 0 ? 'gtk' : core.os.name; // m
 var BOOTSTRAP = this;
 
 function initOstypes() {
+	Services.scriptloader.loadSubScript(core.addon.path.modules + 'ostypes/cutils.jsm', BOOTSTRAP); // need to load cutils first as ostypes_mac uses it for HollowStructure
+	Services.scriptloader.loadSubScript(core.addon.path.modules + 'ostypes/ctypes_math.jsm', BOOTSTRAP);
 	switch (core.os.mname) {
 		case 'winnt':
 		case 'winmo':
@@ -44,48 +46,52 @@ function initOstypes() {
 		default:
 			throw new Error('Operating system, "' + OS.Constants.Sys.Name + '" is not supported');
 	}
-
-	Services.scriptloader.loadSubScript(core.addon.path.modules + 'ostypes/cutils.jsm', BOOTSTRAP);
-	Services.scriptloader.loadSubScript(core.addon.path.modules + 'ostypes/ctypes_math.jsm', BOOTSTRAP);
 }
 
 var OSStuff = {};
 function main() {
 		
-	var jsCallback = function(lpParameter, TimerOrWaitFired) {
-	  console.log('lpParameter:', lpParameter, 'TimerOrWaitFired:', TimerOrWaitFired);
-	  return undefined;
-	};
+	var eventType = ostypes.TYPE.EventTypeSpec();
+	eventType.eventClass = ostypes.CONST.kEventClassKeyboard;
+	eventType.eventKind = ostypes.CONST.kEventHotKeyPressed;
+	
+	ostypes.HELPER.convertLongOSStatus(0x6B657962);
+	
+	console.log('pre install eventType:', eventType.toString(), 'eventType.eventClass:', eventType.eventClass.toString(), 'eventType.eventKind:', eventType.eventKind.toString());
+	
+	var rez_appTarget = ostypes.API('GetApplicationEventTarget')();
+	// console.log('rez_appTarget GetApplicationEventTarget:', rez_appTarget.toString());
+	OSStuff.cHotKeyHandler = ostypes.TYPE.EventHandlerUPP(macHotKeyHandler);
+	console.log('OSStuff.cHotKeyHandler:', OSStuff.cHotKeyHandler.toString());
+	var rez_install = ostypes.API('InstallEventHandler')(rez_appTarget, OSStuff.cHotKeyHandler, 1, eventType.address(), null, null);
+	console.log('rez_install:', rez_install.toString());
+	console.log('OSStuff.cHotKeyHandler:', OSStuff.cHotKeyHandler.toString());
+	// console.log('post install eventType:', eventType.toString(), 'eventType.eventClass:', eventType.eventClass.toString(), 'eventType.eventKind:', eventType.eventKind.toString());
+	
+	var gMyHotKeyRef = ostypes.TYPE.EventHotKeyRef();
+	var gMyHotKeyID = ostypes.TYPE.EventHotKeyID();
+	gMyHotKeyID.signature =  ostypes.TYPE.OSType('1752460081'); // has to be a four char code. MACS is http://stackoverflow.com/a/27913951/1828637 0x4d414353 so i just used htk1 as in the example here http://dbachrach.com/blog/2005/11/program-global-hotkeys-in-cocoa-easily/ i just stuck into python what the stackoverflow topic told me and got it struct.unpack(">L", "htk1")[0]
+	gMyHotKeyID.id = 1876;
+	
+	var rez_appTarget2 = ostypes.API('GetApplicationEventTarget')();
+	console.log('rez_appTarget2 GetApplicationEventTarget:', rez_appTarget2.toString());
+	
+	console.log('gMyHotKeyID:', gMyHotKeyID.toString());
+	console.log('gMyHotKeyID.address():', gMyHotKeyID.address().toString());
+	
+	console.log('ostypes.CONST.shiftKey + ostypes.CONST.cmdKey:', ostypes.CONST.shiftKey + ostypes.CONST.cmdKey);
+	console.log('gMyHotKeyRef.address():', gMyHotKeyRef.address().toString());
+	
+	var rez_reg = ostypes.API('RegisterEventHotKey')(49, ostypes.CONST.shiftKey + ostypes.CONST.cmdKey, gMyHotKeyID, rez_appTarget2, 0, gMyHotKeyRef.address());
+	console.log('rez_reg:', rez_reg.toString());
+	ostypes.HELPER.convertLongOSStatus(rez_reg);
+	
+}
 
-	OSStuff.cCallback = ostypes.TYPE.WAITORTIMERCALLBACK.ptr(jsCallback);
-	
-	var hNewTimer = ostypes.TYPE.HANDLE();
-	OSStuff.hNewTimer = hNewTimer;
-	var ret = ostypes.API('CreateTimerQueueTimer')(
-	  hNewTimer.address(),
-	  null,
-	  OSStuff.cCallback,
-	  null,
-	  5000,
-	  0,
-	  ostypes.CONST.WT_EXECUTEDEFAULT
-	);
-	
-	console.log('ret:', ret, 'winLastError:', ctypes.winLastError);
-	console.log('hNewTimer:', hNewTimer.toString());
-	
-	OSStuff.xpcomTimer = Cc['@mozilla.org/timer;1'].createInstance(Ci.nsITimer);
-	
-	var cleanup = function() {
-		var rez_del = ostypes.API('DeleteTimerQueueTimer')(null, OSStuff.hNewTimer, null);
-		console.log('rez_del:', rez_del);
-		delete OSStuff.hNewTimer;
-		delete OSStuff.cCallback;
-		delete OSStuff.xpcomTimer;
-	};
-	
-	xpcomSetTimeout(OSStuff.xpcomTimer, 10000, cleanup);
-	
+function macHotKeyHandler(nextHandler, theEvent, userDataPtr) {
+	// EventHandlerCallRef nextHandler, EventRef theEvent, void *userData
+	console.error('wooohoo ah!! called hotkey!');
+	return 1; // must be of type ostypes.TYPE.OSStatus
 }
 
 function install() {}
