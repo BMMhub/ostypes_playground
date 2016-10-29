55ec7ccfb7d285dc3f8ee6ed1e50f1388f08ee54
Noitidart <noitidart@gmail.com>
2016-10-29 13:17:53 -0700

    dragging from non drag point on gtk window works

--- bootstrap.js
+++ bootstrap.js
@@ -50,21 +50,144 @@ function initOstypes() {
 
 var OSStuff = {};
 function main() {
-		
+		var win = Services.ww.openWindow(null, 'data:text/html,rawr<style>* { background-color: transparent }</style>', null, 'width=300,height=300', null);
+
+		win.addEventListener('mousedown', function(e) {
+			if (e.button !== 0) return; // drag can only be initated by primary mouse button
+
+			const GDK_BUTTON_PRIMARY = 1; // im not sure about this, it might be specifically "LEFT" button, and not "Primary" as it is in javascript
+
+			// https://dxr.mozilla.org/mozilla-central/source/widget/gtk/nsWindow.cpp#6638
+			var gdkwinptr = ostypes.TYPE.GdkWindow.ptr(ctypes.UInt64(_getNativeHandlePtrStr(win)));
+			var button = ostypes.TYPE.gint();
+			var screenX = ostypes.TYPE.gint();
+			var screenY = ostypes.TYPE.gint();
+
+			var toplevel = ostypes.API('gdk_window_get_toplevel')(gdkwinptr);
+			console.log('pointers same?', cutils.comparePointers(toplevel, gdkwinptr), cutils.jscEqual(toplevel, gdkwinptr), cutils.jscGetDeepest(toplevel), cutils.jscGetDeepest(gdkwinptr));
+
+			var coordmouse = getMouseCoords()
+			console.log('coordmouse:', coordmouse);
+			var coordwin = getWindowCoords(gdkwinptr);
+			console.log('coordwin:', coordwin);
+
+			ostypes.API('gdk_window_begin_move_drag')(gdkwinptr, GDK_BUTTON_PRIMARY, coordmouse.x, coordmouse.y, ostypes.CONST.GDK_CURRENT_TIME);
+		}, false);
+
+	/*
+	// for fun trying gtk callback method - couldnt get it to work, the connection happens, but for some reason its not trigger
+	var gdkwinptr = ostypes.TYPE.GdkWindow.ptr(ctypes.UInt64(_getNativeHandlePtrStr(win)));
+	var gtkwinptr = ostypes.HELPER.gdkWinPtrToGtkWinPtr(gdkwinptr);
+	var gtkwidgetptr = ctypes.cast(gtkwinptr, ostypes.TYPE.GtkWidget.ptr);
+
+	// ostypes.API('gtk_widget_add_events')(gtkwidgetptr, ostypes.CONST.GDK_BUTTON_PRESS_MASK);
+
+	// must keep the handler global until done
+	OSStuff.handler = ostypes.TYPE.GtkWidget_button_press_callback(pressHandler);
+	var connectionid = ostypes.API('g_signal_connect_object')(gtkwidgetptr, 'button-press-event', OSStuff.handler, null, 0);
+	console.log('connectionid:', cutils.jscGetDeepest(connectionid));
+
+	if (cutils.jscEqual(connectionid, 0)) {
+		console.error('failed to connect button_press_event listener');
+		throw new Error('failed to connect button_press_event listener')
+	}
+	*/
+
+}
+
+function pressHandler(widgetPtr, eventPtr, user_data) {
+	// https://developer.gnome.org/gtk3/stable/GtkWidget.html#GtkWidget-button-press-event // gboolean user_function (GtkWidget *widget, GdkEvent *event, gpointer user_data)
+	console.log('in handler');
+
+	return false; //  TRUE to stop other handlers from being invoked for the event. FALSE to propagate the event further.
+}
+
+function getMouseCoords() {
+	if (GTK_VERSION < 3) {
+		// use GTK2 method
+		var x = ostypes.TYPE.gint();
+		var y = ostypes.TYPE.gint();
+
+		var gdkwinptr_undermouse = ostypes.API('gdk_window_get_pointer')(ostypes.API('gdk_get_default_root_window')(), x.address(), y.address(), null);
+
+		return {
+			x: parseInt(cutils.jscGetDeepest(x)),
+			y: parseInt(cutils.jscGetDeepest(y)),
+		};
+	} else {
+		// use GTK3 method
+		var dispptr = ostypes.API('gdk_display_get_default')();
+
+		// get pointer_device_ptr
+		var pointer_device_ptr;
+		try {
+			// try GTK3
+			// will throw `Error: couldn't find function symbol in library  ostypes_x11.jsm:2981:11` if it is deprecated, meaning user is on GTK3.2 and not GTK3
+			var devmgrptr = ostypes.API('gdk_display_get_device_manager')(dispptr);
+			pointer_device_ptr = ostypes.API('gdk_device_manager_get_client_pointer')(devmgrptr);
+		} catch(ex) {
+			// this is future proofing, right now firefox doesnt use GTK3.2
+			console.log('probably GTK3.2, ex:', ex);
+			// use GTK3.2
+			var seatmgr = ostypes.API('gdk_display_get_default_seat')(dispptr);
+			pointer_device_ptr = ostypes.API('gdk_seat_get_pointer')(seatmgr);
+		}
+
+		var x = ostypes.TYPE.gint();
+		var y = ostypes.TYPE.gint();
+		ostypes.API('gdk_device_get_position')(pointer_device_ptr, null, x.address(), y.address());
+
+		return {
+			x: parseInt(cutils.jscGetDeepest(x)),
+			y: parseInt(cutils.jscGetDeepest(y)),
+		};
+	}
+}
+
+function getWindowCoords(aGdkWinPtr) {
+	// gdk_window_get_root_origin: Object { x: 65, y: 486 }  bootstrap.js:150
+	// gdk_window_get_geometry: Object { x: 0, y: 0 }  bootstrap.js:156
+	// gdk_window_get_position: Object { x: 65, y: 514 }
+
+	var x = ostypes.TYPE.gint();
+	var y = ostypes.TYPE.gint();
+
+	ostypes.API('gdk_window_get_root_origin')(aGdkWinPtr,  x.address(), y.address());
+	console.log('gdk_window_get_root_origin:', {
+		x: parseInt(cutils.jscGetDeepest(x)),
+		y: parseInt(cutils.jscGetDeepest(y)),
+	});
+
+	ostypes.API('gdk_window_get_geometry')(aGdkWinPtr, x.address(), y.address(), null, null);
+	console.log('gdk_window_get_geometry:', {
+		x: parseInt(cutils.jscGetDeepest(x)),
+		y: parseInt(cutils.jscGetDeepest(y)),
+	});
+
+	ostypes.API('gdk_window_get_position')(aGdkWinPtr, x.address(), y.address());
+	console.log('gdk_window_get_position:', {
+		x: parseInt(cutils.jscGetDeepest(x)),
+		y: parseInt(cutils.jscGetDeepest(y)),
+	});
+
+	return {
+		x: parseInt(cutils.jscGetDeepest(x)),
+		y: parseInt(cutils.jscGetDeepest(y)),
+	};
 }
 
 function unmain() {
-	
+
 }
 
 function install() {}
 function uninstall() {}
 
 function startup(aData, aReason) {
-	
+
 	initOstypes();
 	main();
-	
+
 }
 
 function shutdown(aData, aReason) {
@@ -80,5 +203,14 @@ function xpcomSetTimeout(aNsiTimer, aDelayTimerMS, aTimerCallback) {
 			aTimerCallback();
 		}
 	}, aDelayTimerMS, Ci.nsITimer.TYPE_ONE_SHOT);
+}
+function _getNativeHandlePtrStr(domWindow) {
+  const baseWindow = domWindow.QueryInterface(Ci.nsIInterfaceRequestor)
+                      .getInterface(Ci.nsIWebNavigation)
+                      .QueryInterface(Ci.nsIDocShellTreeItem)
+                      .treeOwner
+                      .QueryInterface(Ci.nsIInterfaceRequestor)
+                      .getInterface(Ci.nsIBaseWindow);
+  return baseWindow.nativeHandle;
 }
 // end - common helper functions
